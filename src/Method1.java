2282e80dd02fa372574e9c6f367238aea39447c1
FLTOSHI <fltoshimyname@gmail.com>
2023-12-26 21:55:12 +0300

    Initial commit

---
+++ src/Method1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Method1 {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            int c = scanner.nextInt();
+
+            double result = triangle(a, b, c);
+            System.out.println("Результат: " + result);
+        }
+
+        private static double triangle(int a, int b, int c) {
+            int p = (a + b + c);
+            return p;
+        }
+    }
